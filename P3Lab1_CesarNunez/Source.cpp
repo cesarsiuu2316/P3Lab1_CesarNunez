720d1cf49f8f7af8dec4acfb91b43efeda2c2f7a
César Núñez <cesar23augusto16@gmail.com>
2022-04-22 15:34:22 -0600

    teorema de pitágoras funcional

--- P3Lab1_CesarNunez/Source.cpp
+++ P3Lab1_CesarNunez/Source.cpp
@@ -8,7 +8,7 @@ int main() {
 	int resp;
 	char despejar = 'C';
 	cout << "---- Teorema de Pitagoras -----" << endl;
-	cout << "Seleccione incognita: ";
+	cout << "Seleccione incognita (A/B/C): ";
 	int incognita;
 	cin >> incognita;
 	
@@ -16,17 +16,29 @@ int main() {
 		cout << "Ecuacion inicial:\nC^2 = A^2 + B^2" << endl;
 		despejar = 'A';
 		cout << "Despejar para A:\nA^2 = C^2 - B^2\n";
-		cout << "Ingresar valor para C: ";
-		cin >> c;
-		c2 = c * c;
+		
+		//validaciones
 		cout << "Ingresar valor para B: ";
 		cin >> b;
+		while (b < 4 || b >= 16) {
+			cout << "Ingresar un valor para B entre 4 y 15: ";
+			cin >> b;
+		}
 		b2 = b * b;
+		cout << "Ingresar valor para C: ";
+		cin >> c;
+		while (c < 4 || c >= 16 || c == b) {
+			cout << "Ingresar un valor para C mayor que B y menor que 16: ";
+			cin >> c;
+		}
+		c2 = c * c;
+
 		cout << "Ecuacion: A^2 = " << c << "^2 - " << b << "^2\n";
 		cout << "A^2 = " << c2 << " - " << b2 << endl;
 		int resta = c2 - b2;
 		cout << "A^2 = " << resta << endl;
 
+		// verificar que sea raiz cuadrada exacta
 		for (int i = 0; i < resta; i++)
 		{
 			if (i * i == resta) {
@@ -39,27 +51,35 @@ int main() {
 			cout << "A = " << resp << endl;
 		}
 		else {
-			cout << "La resta de la hipotenusa con el cateto no es una raíz exacta.\n";
+			cout << "La resta de la hipotenusa con el cateto no es una raiz exacta.\n";
 		}
 	}
 	else if (incognita == 2) {
 		cout << "Ecuacion inicial:\nC^2 = A^2 + B^2" << endl;
 		despejar = 'B';
 		cout << "Despejar para B:\nB^2 = C^2 - A^2\n";
-		while (c < 4 || c >= 16) {
 
-		}
-		cout << "Ingresar valor para C: ";
-		cin >> c;
-		c2 = c * c;
+		//validaciones
 		cout << "Ingresar valor para A: ";
 		cin >> a;
+		while (a < 4 || a >= 16) {
+			cout << "Ingresar un valor para A entre 4 y 15: ";
+			cin >> a;
+		}
 		a2 = a * a;
+		cout << "Ingresar valor para C: ";
+		cin >> c;
+		while (c < 4 || c >= 16 || c == a) {
+			cout << "Ingresar un valor para C mayor que A y menor que 16: ";
+			cin >> c;
+		}
+		c2 = c * c;
 		cout << "Ecuacion: B^2 = " << c << "^2 - " << a << "^2\n";
 		cout << "B^2 = " << c2 << " - " << a2 << endl;
 		int resta = c2 - a2;
 		cout << "B^2 = " << resta << endl;
 
+		// verificar que sea raiz cuadrada exacta
 		for (int i = 0; i < resta; i++)
 		{
 			if (i * i == resta) {
@@ -69,10 +89,10 @@ int main() {
 			}
 		}
 		if (raizExacta == true) {
-			cout << "A = " << resp << endl;
+			cout << "B = " << resp << endl;
 		}
 		else {
-			cout << "La resta de la hipotenusa con el cateto no es una raíz exacta.\n";
+			cout << "La resta de la hipotenusa con el cateto no es una raiz exacta.\n";
 		}
 	}
 	else if (incognita == 3) {
@@ -81,15 +101,26 @@ int main() {
 		cout << "Ya esta despejada.\nC^2 = A^2 + B^2" << endl;
 		cout << "Ingresar valor para A: ";
 		cin >> a;
+
+		//validaciones
+		while (a < 4 || a >= 16) {
+			cout << "Ingresar un valor para A entre 4 y 15: ";
+			cin >> a;
+		}
 		a2 = a * a;
 		cout << "Ingresar valor para B: ";
 		cin >> b;
+		while (b < 4 || b >= 16) {
+			cout << "Ingresar un valor para B entre 4 y 15: ";
+			cin >> b;
+		}
 		b2 = b * b;
 		cout << "Ecuacion: C^2 = " << a << "^2 + " << b << "^2\n";
 		cout << "C^2 = " << a2 << " + " << b2 << endl;
 		int suma = a2 + b2;
 		cout << "C^2 = " << suma << endl;
 
+		// verificar que sea raiz cuadrada exacta
 		for (int i = 0; i < suma; i++)
 		{
 			if (i * i == suma) {
@@ -99,10 +130,10 @@ int main() {
 			}
 		}
 		if (raizExacta == true) {
-			cout << "A = " << resp << endl;
+			cout << "C = " << resp << endl;
 		}
 		else {
-			cout << "La suma de los catetos son una raiz cuadrada exacta.\n";
+			cout << "La suma de los catetos no son una raiz cuadrada exacta.\n";
 		}
 	}
 	else {
